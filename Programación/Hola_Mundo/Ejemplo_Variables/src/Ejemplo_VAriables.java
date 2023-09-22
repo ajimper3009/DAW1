dd395232c5f1b35d39c3fda4e2ce652aceb6e4f1
ajimper3009 <ajimper3009@g.educaand.es>
2023-09-22 16:58:57 +0200

    Añado Fichero Ejemplo_VAriables

---
+++ Programación/Hola_Mundo/Ejemplo_Variables/src/Ejemplo_VAriables.java
@@ -0,0 +1,27 @@
+
+public class Ejemplo_VAriables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	// Creación de las variables
+		
+		int i = 10;
+		double d = 3.14;
+		char c1 = 'a';
+		char c2 = 'A';
+		boolean encontrado = true;
+		String mensaje = "Bienvenido a java";
+	// Mostrar en consola
+		
+		System.out.println("La variable i es de tipo entero y su valor es: " + i);
+		System.out.println("La variable d es de tipo double y su valor es: " + d);
+		System.out.println("La variable c1 es de tipo char y su valor es: " + c1);
+		System.out.println("La variable c2 es de tipo char y su valor es: " + c2);
+		System.out.println("La variable encontrado es de tipo boolean y su valor es: " + encontrado);
+		System.out.println("La variable mensaje es de tipo String y su valor es: " + mensaje);
+	}
+	
+	
+
+}
