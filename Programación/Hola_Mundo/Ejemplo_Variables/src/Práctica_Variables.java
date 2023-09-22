07095e3404aad34cf103a53655b5132bdd5e24ab
ajimper3009 <ajimper3009@g.educaand.es>
2023-09-22 17:36:22 +0200

    Añado ejercicio de operaciones

---
+++ Programación/Hola_Mundo/Ejemplo_Variables/src/Práctica_Variables.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Práctica_Variables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double d1 = 10;
+		double d2 = 20;
+		double resul;
+		
+		resul = -4 + d1 * 2;
+		resul = (-4 + d1) * 2;
+		resul = (d1 + 2)/12 + d2;
+		resul = (d2/d1)/2;
+		resul = d2/(d1/2);
+//		resul = 
+		
+		System.out.println( "Restamos 4 al doble de d1: " + resul ); 
+		System.out.println( "Restamos 4 a d1 y calculamos el doble: " + resul );
+		System.out.println( "Sumamos 2 a d1 y dividimos por 12, a todo ello sumamos d2: " + resul );
+		System.out.println( "Dividimos d2 entre d1, y todo ello dividido entre 2: " + resul );
+		System.out.println( "Dividimos d2 entre la mitad de d1: " + resul );
+		System.out.println( "Restamos a d2 un cuarto de d1: " + resul );
+		
+		
+		
+		
+		
+			
+			
+		
+		
+	}
+
+}
